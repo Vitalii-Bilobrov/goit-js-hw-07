8a517b64766a4fff40676180719d3cba58798bc1
Vitalii-Bilobrov <106316488+Vitalii-Bilobrov@users.noreply.github.com>
2022-11-06 16:13:31 +0200

    Commit_3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+          data-source="${original}"
+        />
+      </a>
+    </div>`
+    )
+    .join("");
+}
+
+galleryContainer.innerHTML = createGalleryItems(galleryItems);
+
+galleryContainer.addEventListener("click", onGaleryItemCkick);
+
+function onGaleryItemCkick(event) {
+  event.preventDefault();
+  const { target } = event;
+  const selectedItem = target.dataset.source;
+  if (!selectedItem) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+  <div class="modal">
+    <img src="${selectedItem}" width="800" height="600">
+    </div>
+`);
+  instance.show();
+
+  document.addEventListener("keydown", onEscapeClose);
+  function onEscapeClose(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createColorCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryContainer.addEventListener("click", onGaleryItemCkick);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  enableKeyboard: true,
+});
+
+function createColorCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"  />
+      </a>`
+    )
+    .join("");
+}
+function onGaleryItemCkick(event) {
+  event.preventDefault();
+}
