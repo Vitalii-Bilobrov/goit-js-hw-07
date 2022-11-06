ff57593e88c99e58871090878956534cfaa7e0e7
Vitalii-Bilobrov <106316488+Vitalii-Bilobrov@users.noreply.github.com>
2022-11-06 16:21:14 +0200

    Commit_4

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -19,7 +19,7 @@ function createColorCardsMarkup(galleryItems) {
     .map(
       ({ preview, original, description }) =>
         `<a class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}"  />
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"  />
       </a>`
     )
     .join("");
